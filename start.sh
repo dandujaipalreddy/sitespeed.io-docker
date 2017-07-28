ff5dde2bc0f78b9d5c4d9d2f111e80c0783b0f48
Jaipal Reddy <dandujaipalreddy@hotmail.com>
2017-07-28 15:24:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ echo 'Starting Xvfb ...'
 export DISPLAY=:99
 2>/dev/null 1>&2 Xvfb :99  -ac -nolisten tcp -screen 0 1500x1200x16 &
 sleep 1
-exec node --max-old-space-size=$MAX_OLD_SPACE_SIZE /home/app/sitespeed.io/bin/sitespeed.js "$@"
+exec node --max-old-space-size=$MAX_OLD_SPACE_SIZE /home/ppoker/sitespeed.io/bin/sitespeed.js "$@"
